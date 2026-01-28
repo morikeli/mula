eadc16fb3d5ec124a3b72cd45cee69e7065203d2
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-01-28 11:16:25 +0300

    feat: created an auth repo

---
+++ lib/data/repositories/auth_repo.dart
@@ -0,0 +1,51 @@
+import '../../core/services/auth_service.dart';
+import '../models/login_model.dart';
+import '../models/signup_model.dart';
+import '../models/user_model.dart';
+
+class AuthRepository {
+	final AuthService _service;
+
+	AuthRepository(AuthService authService, {AuthService? service}) : _service = service ?? AuthService();
+
+	// Attempts to sign in a user with [email] and [password].
+	// Returns a [LoginModel] on success or throws on failure.
+	Future<LoginModel> getUserCredentials(String email, String password) async {
+		try {
+			final UserModel? user = await _service.login(email, password);
+			if (user == null) throw Exception('Unable to login user');
+			return LoginModel(email: email, password: password);
+		} catch (e) {
+			rethrow;
+		}
+	}
+
+	// Creates a new user account. [username] may be a single name; it will
+	// be used as `firstName` and `lastName` will be empty.
+	Future<SignupModel> createUserAccount(String username, String mobileNumber, String email, String password) async {
+		try {
+			final names = username.split(' ');
+			final firstName = names.isNotEmpty ? names.first : username;
+			final lastName = names.length > 1 ? names.sublist(1).join(' ') : '';
+
+			final UserModel? user = await _service.signup(firstName, lastName, email, '', password);
+			if (user == null) throw Exception('Unable to create account');
+
+			return SignupModel(
+				firstName: firstName,
+				lastName: lastName,
+				email: email,
+				mobileNumber: mobileNumber,
+				pwd: password,
+				confirmPwd: password,
+			);
+		} catch (e) {
+			rethrow;
+		}
+	}
+
+	Future<void> signOut() async => _service.logout();
+
+	Future<void> resetPassword(String email) async => _service.resetPassword(email);
+}
+
