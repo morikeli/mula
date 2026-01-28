842e374e34247072b96ee8fde16001bc36fd70e6
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-01-28 11:26:50 +0300

    migrate: replaced GetX with BLoC for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/get.dart';
+import 'package:maverick_app/presentation/bloc/auth_bloc/auth_bloc.dart';
 import 'package:maverick_app/routes.dart';
-import 'package:toastification/toastification.dart';
 
 import 'core/helpers/firebase_options.dart';
+import 'core/services/auth_service.dart';
+import 'core/services/pin_service.dart';
+import 'core/services/transaction_service.dart';
 import 'core/theme/theme.dart';
+import 'data/repositories/auth_repo.dart';
+import 'data/repositories/pin_repo.dart';
+import 'data/repositories/transaction_repo.dart';
+import 'presentation/bloc/pin_bloc/pin_bloc.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -24,14 +31,28 @@ class MaverickApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ToastificationWrapper(
-      child: GetMaterialApp(
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(create: (context) => AuthRepository(AuthService())),
+        RepositoryProvider(create: (context) => PinRepository(PinService())),
+        // RepositoryProvider(create: (context) => TransactionRepository(TransactionService())),
+
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => AuthBloc(context.read<AuthRepository>())),
+          BlocProvider(create: (context) => PinBloc(context.read<PinRepository>())),
+          // BlocProvider(create: (context) => PinBloc(context.read<PinRepository>())),
+      
+        ],
+        child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Maverick',
         darkTheme: MaverickAppTheme.darkTheme,
         theme: MaverickAppTheme.lightTheme,
         initialRoute: '/onboarding-screen',
         routes: routes,
+        ),
       ),
     );
   }
