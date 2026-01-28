f285a797a52a93b1851682c5c69cf7e26aaa06b2
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-01-28 11:19:13 +0300

    fix: refactored `SignupScreen()` widget

---
+++ lib/presentation/views/auth/signup_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../core/utils/app_toasts.dart';
+import '../../../core/utils/loading_indicators.dart';
+import '../../bloc/auth_bloc/auth_bloc.dart';
+import '../../widgets/common/footer.dart';
+import '../../widgets/forms/signup_form.dart';
+
+
+class SignupScreen extends StatelessWidget {
+  static String routeName = '/signup';
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is IsAuthenticated) {
+            Navigator.pushNamed(context, '/homescreen');
+          } else if (state is AuthFailed) {
+            AppToast.showError(context, title: state.errorMessage.toString());
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return Center(child: AppLoadingIndicators.loadingIndicatorLarge());
+          }
+          
+          return SafeArea(
+            child: SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: ListView(
+                  children: [
+                    signupScreenTitle(context),
+                    const SizedBox(height: 8.0),
+                    signupScreenSubTitle(context),
+                    SizedBox(height: 16.0),
+                    SignupForm(),
+                    SizedBox(height: 24.0),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      persistentFooterAlignment: AlignmentDirectional.center,
+      persistentFooterButtons: [
+        Footer(
+          primaryText: "Already have an account? ",
+          redirectText: "Login",
+          redirectTo: TapGestureRecognizer()
+            ..onTap = () => Navigator.pushNamed(context, '/login'),
+        ),
+      ],
+    );
+  }
+
+  Text signupScreenSubTitle(BuildContext context) {
+    return Text(
+      'Simplify your crypto payments with us',
+      style: Theme.of(context).textTheme.titleSmall,
+      textAlign: TextAlign.start,
+    );
+  }
+
+  Text signupScreenTitle(BuildContext context) {
+    return Text(
+      'Create Account',
+      style: Theme.of(context).textTheme.titleLarge,
+      textAlign: TextAlign.start,
+    );
+  }
+}
