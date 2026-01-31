b42104807b6befee9bdcc5b21d079bccb5d23745
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-01-31 13:56:58 +0300

    fix: updated green color used in the app and `kContainerLightColor`

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-const Color kAppBarColor = Color(0xFF004D40);
-const Color kPrimaryColor = Color(0xFF004D40);
+const Color kAppBarColor = Color(0xFF11BD6A);
+const Color kPrimaryColor = Color(0xFF11BD6A);
 const Color kSecondaryColor = Color(0xFF9E9E9E);
 const Color kScaffoldBgLightColor = Color(0xFFFFFFFF);
 const Color kScaffoldBgDarkcolor = Color(0xFF000000);
-const Color kContainerLightColor = Color(0xCCE0E0E0);
-const Color kContainerDarkColor = Color(0xFF004D40);
-const Color kButtonDarkColor = Color(0xFF004D40);
-const Color kIconDarkColor = Color(0xFF004D40);
+final Color kContainerLightColor = const Color(0xFFD3D2D2).withOpacity(0.25);
+const Color kContainerDarkColor = Color(0xFF11BD6A);
+const Color kButtonDarkColor = Color(0xFF11BD6A);
+const Color kIconDarkColor = Color(0xFF11BD6A);
 const Color kIconLightColor = Color(0xFFFFFFFF);
 const Color kIconDangerColor = Color(0xFFCC180B);
 const Color kTextLightColor = Color(0xFFFFFFFF);
-const Color kTextGreenColor = Color(0xFF004D40);
+const Color kTextGreenColor = Color(0xFF11BD6A);
 const Color kTextDarkColor = Color(0xFF000000);
 const Color kTextSecondaryColor = Color(0xFF8B8B8B);
 const Color kTextFormFieldLabelColor = Color(0xFF009688);
