c4949e48e96c1087cd4e5292d6dac5979dd1393e
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-01-31 18:55:17 +0300

    feat: created a screen to request password reset code

---
+++ lib/presentation/views/auth/forgot_password_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../../widgets/appbar.dart';
+import '../../widgets/forms/forgot_password_form.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  static String routeName = '/forgot-password';
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(appBarTitle: 'Forgot password'),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView(
+          children: [
+            const SizedBox(height: 12.0),
+            Text(
+              'Enter your email to recieve your password reset code.',
+              style: Theme.of(context).textTheme.titleSmall,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16.0),
+            ForgotPasswordForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
