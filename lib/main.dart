6b6ac70832bd380644f2ef31d742c2df9091f990
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-02 13:05:10 +0300

    refactor: reorder imports for better organization and clarity

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:maverick_app/presentation/widgets/auth_gate.dart';
-import 'package:maverick_app/presentation/views/onboarding_screen.dart';
-import 'package:maverick_app/routes.dart';
 
 import 'core/helpers/firebase_options.dart';
 import 'core/helpers/prefs.dart';
 import 'core/theme/theme.dart';
 import 'core/providers/repository_providers.dart';
 import 'core/providers/bloc_providers.dart';
+import 'presentation/views/onboarding_screen.dart';
+import 'presentation/widgets/auth_gate.dart';
+import 'presentation/widgets/connectivity_listener.dart';
+import 'routes.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
