9c9968075f128505a5dd0cb823896dae5ed8a11c
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-05 10:45:32 +0300

    fix: imported necessary modules

--- lib/presentation/views/transactions/transaction_history_screen.dart
+++ lib/presentation/views/transactions/transaction_history_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../bloc/transaction_bloc/transactions_bloc.dart';
 import '../../widgets/common/appbar.dart';
 import '../../widgets/common/notification_bell_icon.dart';
 import '../../widgets/common/recent_transactions.dart';
