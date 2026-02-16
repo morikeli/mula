15a2099129570aa820d437f72fd9b0c5196ba899
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-16 12:04:33 +0300

    feat: appended NotificationBloc to providers

--- lib/core/providers/bloc_providers.dart
+++ lib/core/providers/bloc_providers.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/repositories/auth_repo.dart';
+import '../../data/repositories/notification_repo.dart';
 import '../../data/repositories/pin_repo.dart';
 import '../../data/repositories/transaction_repo.dart';
 import '../../presentation/bloc/auth_bloc/auth_bloc.dart';
+import '../../presentation/bloc/notifications_bloc/notifications_bloc.dart';
 import '../../presentation/bloc/pin_bloc/pin_bloc.dart';
 import '../../presentation/bloc/transaction_bloc/transactions_bloc.dart';
 import '../../presentation/cubits/connectivity/connectivity_cubit.dart';
@@ -32,6 +34,12 @@ class AppBlocProviders extends StatelessWidget {
             repository: context.read<TransactionRepository>(),
           ),
         ),
+        BlocProvider(
+          create: (context) =>
+              NotificationBloc(context.read<NotificationRepository>()),
+        ),
+
+        // 2. Cubit
         BlocProvider(
           create: (context) =>
               ConnectivityCubit(context.read<ConnectivityService>()),
