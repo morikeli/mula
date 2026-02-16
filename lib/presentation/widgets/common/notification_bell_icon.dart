b42051b1a8027c9669b1220c72466a31619e30d6
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-16 12:17:32 +0300

    feat: updated notification icon to alert users using a badge

--- lib/presentation/widgets/common/notification_bell_icon.dart
+++ lib/presentation/widgets/common/notification_bell_icon.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/theme/colors.dart';
+import '../../bloc/notifications_bloc/notifications_bloc.dart';
 
 class NotificationBellIcon extends StatelessWidget {
   const NotificationBellIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        IconButton(
-          onPressed: () => Navigator.pushNamed(context, '/notifications'),
-          icon: Icon(
-            CupertinoIcons.bell_fill,
-            color: kIconLightColor,
-            size: 24.0,
-          ),
-        ),
-        Positioned(
-          right: 14,
-          top: 16,
-          child: Badge(
-            backgroundColor: kNotificationBadgeColor,
-            smallSize: 8.0,
-            largeSize: 16.0,
-          ),
-        ),
-      ],
+    return BlocBuilder<NotificationBloc, NotificationsState>(
+      builder: (context, state) {
+        int unreadNotificationCount = 0;
+
+        if (state is NotificationBadgeState) {
+          unreadNotificationCount = state.count;
+        }
+
+        return Stack(
+          children: [
+            IconButton(
+              onPressed: () {
+                context.read<NotificationBloc>().add(LoadingNotifications());
+                Navigator.pushNamed(context, '/notifications');
+              },
+              icon: Icon(
+                CupertinoIcons.bell_fill,
+                color: kIconLightColor,
+                size: 24.0,
+              ),
+            ),
+
+            if (unreadNotificationCount > 0)
+              Positioned(
+                right: 14,
+                top: 16,
+                child: Badge(
+                  backgroundColor: kNotificationBadgeColor,
+                  smallSize: 8.0,
+                  largeSize: 16.0,
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
