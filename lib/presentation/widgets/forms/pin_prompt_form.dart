24cd7b8a12feef11e26ab9c0625ddcded5a52caf
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-16 12:48:57 +0300

    feat: updated UI to use pinput instead of a form

--- lib/presentation/widgets/forms/pin_prompt_form.dart
+++ lib/presentation/widgets/forms/pin_prompt_form.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pinput/pinput.dart';
 
-import '../../../core/helpers/form_validation.dart';
+import '../../../core/theme/colors.dart';
 import '../../bloc/pin_bloc/pin_bloc.dart';
-import '../common/form_field.dart';
 
 class PINPromptForm extends StatefulWidget {
   const PINPromptForm({super.key});
@@ -14,65 +13,65 @@ class PINPromptForm extends StatefulWidget {
 }
 
 class _PINPromptFormState extends State<PINPromptForm> {
-  final TextEditingController pinInputController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _pinController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void dispose() {
-    pinInputController.dispose();
+    _pinController.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            pinTextField(),
-            SizedBox(height: 12.0),
-            unlockBtn(context),
-          ],
-        ),
+    final defaultTheme = PinTheme(
+      width: 72,
+      height: 64,
+      textStyle: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.grey.shade300),
       ),
     );
-  }
 
-  Row unlockBtn(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: ElevatedButton(
-            onPressed: () async {
-              final form = _formKey.currentState;
-
-              if (form != null && form.validate()) {
-                final userPin = pinInputController.text.trim();
-
-                context.read<PinBloc>().add(VerifyPinRequested(userPin));
-              }
-            },
-            child: Text('Unlock'),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.12,
+        vertical: 16.0,
+      ),
+      child: ListView(
+        children: [
+          // Screen header
+          Text(
+            'Keep your PIN secure and do not share it with anyone.',
+            textAlign: TextAlign.center,
           ),
-        ),
-      ],
-    );
-  }
+          SizedBox(height: MediaQuery.of(context).size.height * .16),
+          // PIN input field
+          Pinput(
+            length: 4,
+            controller: _pinController,
+            focusNode: _focusNode,
+            obscureText: true,
+            autofocus: true,
+            defaultPinTheme: defaultTheme,
+            focusedPinTheme: defaultTheme.copyWith(
+              decoration: defaultTheme.decoration!.copyWith(
+                border: Border.all(color: kPrimaryColor, width: 2),
+              ),
+            ),
+            submittedPinTheme: defaultTheme,
+            showCursor: true,
+            onCompleted: (pin) {
+              context.read<PinBloc>().add(VerifyPinRequested(pin));
+            },
+          ),
 
-  CustomTextFormField pinTextField() {
-    return CustomTextFormField(
-      controller: pinInputController,
-      label: 'Enter your PIN',
-      icon: CupertinoIcons.circle_grid_3x3,
-      obscureText: true,
-      validator: (value) {
-        return FormValidation.validatePIN(
-          value,
-          pinInputController.text.trim(),
-        );
-      },
+          const SizedBox(height: 24),
+        ],
+      ),
     );
   }
 }
