d3609ec018b49e50573f49081b5b9232142807e6
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-16 11:55:39 +0300

    fix: moved into a dir 📁

---
+++ lib/presentation/views/notifications/notifications_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../core/theme/colors.dart';
+import '../../../core/utils/loading_indicators.dart';
+import '../../bloc/notifications_bloc/notifications_bloc.dart';
+import '../../widgets/common/appbar.dart';
+import 'widgets/notifications_tile.dart';
+
+class NotificationsScreen extends StatelessWidget {
+  static String routeName = '/notifications';
+  const NotificationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(appBarTitle: 'Notifications'),
+      body: BlocBuilder<NotificationBloc, NotificationsState>(
+        builder: (context, state) {
+          if (state is NotificationsLoading) {
+            return Center(child: AppLoadingIndicators.loadingIndicatorLarge());
+          }
+
+          if (state is NotificationLoaded) {
+            if (state.notifications.isEmpty) {
+              return const Center(child: Text('No notifications yet'));
+            }
+
+            return NotificationsTiles(state: state);
+          }
+
+          return ErrorWidget();
+        },
+      ),
+    );
+  }
+}
+
+
+class ErrorWidget extends StatelessWidget {
+  const ErrorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height * 0.32),
+        Icon(
+          CupertinoIcons.bell_slash_fill,
+          size: 48.0,
+          color: kIconLightColor,
+        ),
+        Center(child: Text('No notifications available')),
+      ],
+    );
+  }
+}
