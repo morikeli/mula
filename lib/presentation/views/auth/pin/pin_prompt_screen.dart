0f23c890ac216802e96df44dc4f4c9422b33f7c3
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-17 15:04:54 +0300

    fix: deleted unused import stmts 🚮

--- lib/presentation/views/auth/pin/pin_prompt_screen.dart
+++ lib/presentation/views/auth/pin/pin_prompt_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:vibration/vibration.dart';
-import 'package:vibration/vibration_presets.dart';
 
 import '../../../../core/helpers/app_vibrations.dart';
 import '../../../../core/utils/app_toasts.dart';
