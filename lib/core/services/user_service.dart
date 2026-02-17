773d3d1ff50dfd7180020cac2c430d10c7a544bf
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-17 11:54:58 +0300

    feat: updated `UserService` to update user profile and user profile pic

--- lib/core/services/user_service.dart
+++ lib/core/services/user_service.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 import '../../data/models/user_model.dart';
 
 import 'db_service.dart';
 
 class UserService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
   Future<UserModel?> getUser() async {
     final db = await DBService.database;
     final res = await db.query('user', where: 'uid = ?', whereArgs: [1]);
@@ -11,18 +21,28 @@ class UserService {
     return null;
   }
 
-  Future<void> updateUser(UserModel user) async {
-    final db = await DBService.database;
-    final exists = await getUser();
-    if (exists == null) {
-      await db.insert('user', user.toMap());
-    } else {
-      await db.update(
-        'user',
-        user.toMap(),
-        where: 'uid = ?',
-        whereArgs: [user.uid],
-      );
-    }
+  Future<String?> uploadProfilePic(File file) async {
+    final uid = _auth.currentUser!.uid;
+
+    final ref = _storage.ref().child('profile_pics/$uid.jpg');
+    await ref.putFile(file);
+
+    return await ref.getDownloadURL();
+  }
+
+  Future<void> updateProfile({
+    required String? username,
+    required String? firstName,
+    required String? lastName,
+    String? photoUrl,
+  }) async {
+    final uid = _auth.currentUser!.uid;
+
+    await _db.collection('users').doc(uid).update({
+      'username': username,
+      'firstName': firstName,
+      'lastName': lastName,
+      if (photoUrl != null) 'photoUrl': photoUrl,
+    });
   }
 }
