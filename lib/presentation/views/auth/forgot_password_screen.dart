6533456fc909c813fa720ada9815847b84775599
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-18 11:23:57 +0300

    feat: wrapped `Padding()` with a `BlocConsumer()`

--- lib/presentation/views/auth/forgot_password_screen.dart
+++ lib/presentation/views/auth/forgot_password_screen.dart
@@ -10,20 +10,35 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(appBarTitle: 'Forgot password'),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView(
-          children: [
-            const SizedBox(height: 12.0),
-            Text(
-              'Enter your email to recieve your password reset code.',
-              style: Theme.of(context).textTheme.titleSmall,
-              textAlign: TextAlign.center,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is PasswordResetSuccess) {
+            AppToast.showSuccess(context, title: "Password reset email sent successfully!");
+          } else if (state is PasswordResetFailure) {
+            AppToast.showError(context, title: "${state.error}!");
+          }
+          
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return Center(child: AppLoadingIndicators.loadingIndicatorLarge());
+          }
+          return Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ListView(
+              children: [
+                const SizedBox(height: 12.0),
+                Text(
+                  'Enter your email to recieve your password reset code.',
+                  style: Theme.of(context).textTheme.titleSmall,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16.0),
+                ForgotPasswordForm(),
+              ],
             ),
-            const SizedBox(height: 16.0),
-            ForgotPasswordForm(),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
