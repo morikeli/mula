84fc233e7c903039895f134701346bfcc906ff83
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-19 09:52:23 +0300

    bug-fix: state not updated when transacting money. Wrapped body with a BlocConsumer to fix this.

--- lib/presentation/views/transactions/transaction_screen.dart
+++ lib/presentation/views/transactions/transaction_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../core/utils/app_toasts.dart';
+import '../../../core/utils/loading_indicators.dart';
+import '../../bloc/transaction_bloc/transactions_bloc.dart';
 import '../../widgets/common/appbar.dart';
 import '../../widgets/forms/transaction_form.dart';
 
@@ -10,12 +14,28 @@ class TransactionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        appBarTitle: 'Send money',
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-        child: TransactionForm(),
+      appBar: CustomAppBar(appBarTitle: 'Send money'),
+      body: BlocConsumer<TransactionsBloc, TransactionsState>(
+        listener: (context, state) {
+          if (state is TransactionFailed) {
+            AppToast.showError(context, title: 'Transaction error!', message: state.errorMessage.toString());
+          } else if (state is TransactionSuccess) {
+            AppToast.showSuccess(context, title: 'Money sent successfully!');
+          }
+        },
+        builder: (context, state) {
+          if (state is TransactionLoading) {
+            return Center(child: AppLoadingIndicators.loadingIndicatorLarge());
+          }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12.0,
+              vertical: 12.0,
+            ),
+            child: TransactionForm(),
+          );
+        },
       ),
     );
   }
