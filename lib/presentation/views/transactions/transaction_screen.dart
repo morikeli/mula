b022e688d201f2c7aebfbb18d36e7da7608d3e1f
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-20 14:51:16 +0300

    refactor: updated bloc and state

--- lib/presentation/views/transactions/transaction_screen.dart
+++ lib/presentation/views/transactions/transaction_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/utils/app_toasts.dart';
 import '../../../core/utils/loading_indicators.dart';
-import '../../bloc/transaction_bloc/transactions_bloc.dart';
+import '../../bloc/profile_bloc/profile_bloc.dart';
 import '../../widgets/common/appbar.dart';
 import '../../widgets/forms/transaction_form.dart';
 
@@ -14,17 +14,11 @@ class TransactionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(appBarTitle: 'Send money'),
-      body: BlocConsumer<TransactionsBloc, TransactionsState>(
-        listener: (context, state) {
-          if (state is TransactionFailed) {
-            AppToast.showError(context, title: 'Transaction error!', message: state.errorMessage.toString());
-          } else if (state is TransactionSuccess) {
-            AppToast.showSuccess(context, title: 'Money sent successfully!');
-          }
-        },
+      appBar: CustomAppBar(appBarTitle: 'Search for users'),
+      body: BlocConsumer<ProfileBloc, ProfileState>(
+        listener: (context, state) {},
         builder: (context, state) {
-          if (state is TransactionLoading) {
+          if (state is UserSearchLoading) {
             return Center(child: AppLoadingIndicators.loadingIndicatorLarge());
           }
 
