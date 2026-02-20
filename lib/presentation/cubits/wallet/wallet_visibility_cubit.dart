f5e5664e5c23bf0da89dd1448ac048eb322cfe58
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-20 08:03:20 +0300

    feat: wallet balance is hidden by default

--- lib/presentation/cubits/wallet/wallet_visibility_cubit.dart
+++ lib/presentation/cubits/wallet/wallet_visibility_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WalletVisibilityCubit extends Cubit<bool> {
-  WalletVisibilityCubit() : super(true);
+  WalletVisibilityCubit() : super(false);
 
   void toggle() => emit(!state);
 }
