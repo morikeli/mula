8440334b9659a40c062087a7d2d8b08efb95f7f2
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-20 08:04:46 +0300

    deleted comments for routes and import stmts 🚮

--- lib/routes.dart
+++ lib/routes.dart
@@ -7,28 +7,19 @@ import 'presentation/views/auth/forgot_password_screen.dart';
 import 'presentation/views/auth/login_screen.dart';
 import 'presentation/views/auth/pin/pin_prompt_screen.dart';
 import 'presentation/views/auth/pin/pin_setup_screen.dart';
-// import 'presentation/views/auth/pin/reset_pin.dart';
 import 'presentation/views/onboarding_screen.dart';
-// import 'presentation/views/auth/otp/otp_screen.dart';
-// import 'presentation/views/auth/reset-password/reset_password_screen.dart';
 import 'presentation/views/auth/signup_screen.dart';
-// import 'presentation/views/profile/edit_profile.dart';
 import 'presentation/views/profile/profile_screen.dart';
-// import 'presentation/views/transaction/transaction_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
   LoginScreen.routeName: (context) => const LoginScreen(),
   ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
-  // ResetPasswordScreen.routeName: (context) => const ResetPasswordScreen(),
   SignupScreen.routeName: (context) => const SignupScreen(),
-  // OtpScreen.routeName: (context) => const OtpScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
-  // EditProfileScreen.routeName: (context) => EditProfileScreen(),
   TransactionScreen.routeName: (context) => TransactionScreen(),
   PinSetupScreen.routeName: (context) => PinSetupScreen(),
-  // ResetPinScreen.routeName: (context) => ResetPinScreen(),
   PINScreen.routeName: (context) => PINScreen(),
   NotificationsScreen.routeName: (context) => NotificationsScreen(),
 };
